1e805e93f58f667eab6ee6ae62fe549ba7eec579
Aniket Rai <aniketrai025@gmail.com>
2022-04-22 19:11:11 +1200

    Timer implemented :sparkles:

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,36 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Main.css';
 
 function Main() {
-  /*
-    TODO: Implement the functionality in the following components:
-    - NameEntry textarea.
-    - Set Name button.
-    - TimeInSeconds update.
-    - Start button.
-    - Pause button.
-    - Reset button.
-  */
-
   document.title = "Stopwatch"
+
+  // States
+  const [user, setUser] = useState<string>("User");
+  const [time, setTime] = useState<number>(0);
+  const [timerStarted, setTimerStarted] = useState<boolean>(false);
+
+  useEffect(() => {
+    let interval : NodeJS.Timer = setInterval(()=>{});
+    if (timerStarted) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 0.01);
+      }, 10);
+    } else if (!timerStarted) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timerStarted]);
+
+  const updateUserName = () => {
+    const str = (document.getElementById("NameEntry") as HTMLInputElement).value;
+    if (!(/^\s*$/.test(str))) {
+      setUser(str);
+      (document.getElementById("NameEntry") as HTMLInputElement).value = ""
+    }
+  }
+
+  const startTimer = () => {
+    timerStarted ? document.getElementById("ResetButton")?.removeAttribute("Disabled") : document.getElementById("ResetButton")?.setAttribute("Disabled", "true");
+    setTimerStarted(!timerStarted)
+  }
 
   return (
     <div className="MainPage">
       <div className="WelcomeSection">
         <h1>Stopwatch Application.</h1>
         <div className="NameSetting">
-          <textarea data-testid="NameEntry" className="NameEntry" placeholder="Insert a name you want to use!"/>
-          <button data-testid="SetNameButton" className="SetNameButton">Set Name</button>
+          <textarea data-testid="NameEntry" id="NameEntry" className="NameEntry" placeholder="Insert a name you want to use!" maxLength={32}/>
+          <button data-testid="SetNameButton" className="SetNameButton" onClick={updateUserName}>Set Name</button>
         </div>
-        <h2 data-testid="WelcomeBanner" className="WelcomeBanner">Welcome User!</h2>
+        <h2 data-testid="WelcomeBanner" className="WelcomeBanner">Welcome {user}!</h2>
       </div>
       <div className="TimerMainFrame">
         <h3>Time in seconds:</h3>
-        <h1 data-testid="TimeInSeconds" className="TimeInSeconds">0.00</h1>
+        <h1 data-testid="TimeInSeconds" className="TimeInSeconds">{time.toFixed(2)}</h1>
       </div>
       <div className="TimerButtons">
-        <button data-testid="StartButton" className="StartButton">Start</button>
-        <button data-testid="ResetButton" className="ResetButton">Reset</button>
+        <button data-testid="StartButton" className="StartButton" id="StartButton" onClick={startTimer}>{timerStarted ? "Pause" : "Start"}</button>
+        <button data-testid="ResetButton" className="ResetButton" id="ResetButton" onClick={() => setTime(0)}>Reset</button>
       </div>
     </div>
   );
