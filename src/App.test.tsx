2431e8b4d99dab2c4b63ca5991b836defc05e37a
KenObi <48312022+DragonMeme@users.noreply.github.com>
2022-04-22 17:45:34 +1200

    Update a function.
    Remove the 1+ that caused an edge case generation of 32.01+ number generated. 
    Now should generate 0 <= x < 32.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -89,7 +89,7 @@ describe('Name Setting Functionality.', () => {
 
     const textEntry = screen.getByTestId('NameEntry');
 
-    const entry = generateRandomString(1 + Math.random() * 32);
+    const entry = generateRandomString(Math.random() * 32);
     userEvent.type(textEntry, entry);
     fireEvent.click(setNameButton);
 
@@ -116,7 +116,7 @@ describe('Name Setting Functionality.', () => {
     const setNameButton = screen.getByTestId('SetNameButton');
     const textEntry = screen.getByTestId('NameEntry');
 
-    const entry = generateRandomString(1 + Math.random() * 32);
+    const entry = generateRandomString(Math.random() * 32);
     userEvent.type(textEntry, entry);
     fireEvent.click(setNameButton);
 
