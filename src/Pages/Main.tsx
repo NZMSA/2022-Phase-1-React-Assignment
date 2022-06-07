c0b0a6c3881517c5d5fa20931b613a412b336b94
TylerDonghwi <tylerdonghwikim@email.com>
2022-06-07 16:21:24 +1200

    Name Entry Implemented

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.css';
 
 function Main() {
@@ -14,15 +14,26 @@ function Main() {
 
   document.title = "Stopwatch"
 
+  // NameEntry
+  const [name, setName] = useState("User")
+
+  const updateUserName = () => {
+    const newUserName = (document.getElementById("NameEntry") as HTMLTextAreaElement).value
+    if (newUserName.length > 0 && newUserName !== null) {
+      setName(newUserName);
+      (document.getElementById("NameEntry") as HTMLTextAreaElement).value = "";
+    }
+  }
+
   return (
     <div className="MainPage">
       <div className="WelcomeSection">
         <h1>Stopwatch Application.</h1>
         <div className="NameSetting">
-          <textarea data-testid="NameEntry" className="NameEntry" placeholder="Insert a name you want to use!"/>
-          <button data-testid="SetNameButton" className="SetNameButton">Set Name</button>
+          <textarea data-testid="NameEntry" id="NameEntry" className="NameEntry" placeholder="Insert a name you want to use!" maxLength={32} />
+          <button data-testid="SetNameButton" className="SetNameButton" onClick={updateUserName}>Set Name</button>
         </div>
-        <h2 data-testid="WelcomeBanner" className="WelcomeBanner">Welcome User!</h2>
+        <h2 data-testid="WelcomeBanner" id="userName" className="WelcomeBanner">Welcome {name}!</h2>
       </div>
       <div className="TimerMainFrame">
         <h3>Time in seconds:</h3>
